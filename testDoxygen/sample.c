9dcad8cf8c1641804f7970068bdfdb794de168b6
Takamasa Mioki <t.mioki@gmail.com>
2012-03-07 23:46:11 +0900

    add files

---
+++ testDoxygen/sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+
+*/
+
+int myAdd(int a, int b)
+{
+  return a + b;
+}
+
+int main(void)
+{
+  printf("hello![%d]\n", myAdd(1,2));
+
+  return 0;
+}
+
---
+++ testDoxygen/sample.h
@@ -0,0 +1,8 @@
+#define MAX(a,b) (((a)>(b))?(a):(b))
+typedef unsigned int UINT32;
+int errno;
+int open(const char *,int);
+int close(int);
+size_t write(int,const char *, size_t);
+int read(int,char *,size_t);
+
